d33d091692449b1fc93b174ef96c5d79e32ee119
jonofarc <jonathanm@itworks.com>
2021-11-22 22:10:58 -0500

    add lives and debug speed settings

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,74 @@ using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
-    private int  blocksInPlay =0;
+    public int lives = 0;
+    private int blocksInPlay = 0;
+    private int ballsInPlay = 0;
+    public GameObject ball;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        updateBalls(0);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        if (Debug.isDebugBuild) {
 
+            if (Input.GetKey(KeyCode.Alpha1)){
+                updateTimeScale(1.0f);
+            }
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                updateTimeScale(2.0f);
+            }
+            if (Input.GetKey(KeyCode.Alpha3))
+            {
+                updateTimeScale(3.0f);
+            }
 
+        }
+
+    }
+
+    private void updateTimeScale(float scale)
+    {
+        Time.timeScale = scale;
     }
 
     void updateBalls(int amount)
     {
         GlobalVariables.ballsInPlay += amount;
         if (GlobalVariables.ballsInPlay <= 0) {
-            StartCoroutine(gameOver());
+            if (lives <= 0) {
+                StartCoroutine(gameOver());
+            }
+            else {
+                //respawn a new ball and lower lives count
+                if (amount == 0){
+                    StartCoroutine(spawnNewBall(0));
+                }
+                else {
+                    StartCoroutine(spawnNewBall(3));
+                }
+                
+               
+                lives--;
+                GlobalVariables.ballsInPlay += 1;
+
+            }
+            
         }
+    }
+
+    private IEnumerator spawnNewBall(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Vector3 possition = new Vector3(5.0f, 1.0f, 0.6f);
+        GameObject newBall = Instantiate(ball, possition, ball.transform.rotation) as GameObject;
     }
 
     void updateBlocks(int amount)
@@ -53,5 +102,7 @@ public class GameManagerScript : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
+
+    //Debug section
 
 }
--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -7,8 +7,9 @@ public class GlobalVariables : MonoBehaviour {
 	public static string mainCameraTag = "MainCamera";
 	public static string blockTag = "Block";
 	public static string playerTag = "Player";
-	public static int ballsInPlay = 1;
+	public static int ballsInPlay = 0;
 	public static int blocksInPlay = 0;
+	
 	// ball damage is handled as a negative value as it is making damage and reducing block hp
 	public static int ballDamage = 1;
 
